70355949584cf6dfec3b05a29920ad9a7c13625f
marcuspgraham <64594348+marcuspgraham@users.noreply.github.com>
2020-10-14 11:55:14 +0100

    14_10_20_Update 1

--- gameShow.js
+++ gameShow.js
@@ -35,8 +35,12 @@ function addPhraseToDisplay(phrases) {
     for (var i = 0; i < phrases.length; i++) {
         const list = document.createElement('li');
         list.innerHTML = phrases[i];
-        document.querySelector('#phrase ul').appendChild(list).classList.add('letter');
-        
+        document.querySelector('#phrase ul').appendChild(list)
+        if (phrases[i] !== '') {
+            list.className = 'letter';
+        } else {
+            list.className = 'space';
+        }
     }
 }
 
@@ -49,41 +53,88 @@ function checkletter(startButton) {
     // store all elements with the class .letter
     const liElements = document.querySelectorAll('.letter');
     // create variable match
-    const matchFound = null;
+    let matchFound = null;
     // loop over all .letters and see if they match the clickedLetter
     for (var i = 0; i < liElements.length; i++) {
         // if clickedLetter === .letter 
         if (startButton === liElements[i].textContent) {
             // add .show class to the .letter
-            document.addClass('show');
+            liElements[i].classList.add('show');
             // store the clickedLetter in match variable.
-            const match = liElements[i].textContent
-            // return match;
-            return matchFound;
+            matchFound = liElements[i].textContent;
+            
     }
 }
+        // return match;
+    return matchFound;
+}
 
-checkletter(addPhraseToDisplay(phrases));
-
-
-    
-/* check if the game has been won or lost 
-the event listener should be listening for 
-a user to press a button on the on
-screen keyboard, not the physical keyboard 
-of the computer */
 
 // listen for the on screen keyboard to be clicked
 qwerty.addEventListener('click', (e) => {
+
     // filter out clicks that dont happen on buttons
     if (e.target.tagName === 'BUTTON') {
-        // add chosen class to pressed button
-        btn.classList.addClass('chosen');
-        // call checkletter - store results in a variable
-        let pushButton = checkletter;
-        // if no letter found remove one heart, increment missed counter
-} else {
 
+        // add chosen class to pressed button
+        e.target.classList.add('chosen');
+        
+        if (e.target.className == 'chosen') {
+            e.target.disabled = true;
+        }
+
+        // call checkletter - store results in a variable
+        let letterFound = checkletter(e.target.textContent);
+
+        // if no letter found remove one heart, increment missed counter
+        if (letterFound === null) {  
+            let liveHeart = document.querySelectorAll('.tries');
+            let lostHeart = document.querySelectorAll('.tries img');
+            lostHeart[0].src = 'images/lostHeart.png';
+            liveHeart[0].className = '';
+        } 
+ 
 }
+    checkWin()  
 });
 
+
+
+// check if the game is won or lost
+
+function checkWin () {
+
+    // Create a variable to store the li elements that have the class name “letter”
+    let letterOne = document.querySelectorAll('.letter');
+
+    // Create a variable to store the li elements that have the class name “show”
+    let show = document.querySelectorAll('.show');
+
+    // Check if the length of the 2 variables are the same. If they are, display the win overlay
+    if (letterOne || show) {
+
+    // Create the win overlay by adding the “win” class to the start overlay.
+    overlay.className = 'win';
+
+    // Change the headline text of the start overlay to show a person won.
+    document.querySelector('h2.title').innerHTML = 'You Win';
+
+    //Change the display property of the overlay to “flex”
+    overlay.style.display = 'flex';
+
+    }
+
+    // Check if the missed counter is greater than 4. If they are, display the lose overlay
+    if (missed >= 5) {
+
+    // Create the lose overlay by adding the “lose” class to the start overlay.
+    overlay.className = 'lose';
+    
+    // Change the headline text of the start overlay to show a person lost.
+    document.querySelector('h2.title').innerHTML = 'You Lose';
+
+    // Change the display property of the overlay to “flex”
+    overlay.style.display = 'flex';
+
+    }
+}
